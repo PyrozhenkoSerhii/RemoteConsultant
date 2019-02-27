cfbc46090e8ed03eaa15811c6774e78664d517bf
PyrozhenkoSerhii <serhii.pyrozhenko@nure.ua>
2019-02-27 23:05:48 +0200

    controller fixed

--- server/controllers/company.js
+++ server/controllers/company.js
@@ -7,28 +7,29 @@ import wrap from '../middlewares/wrap'
 const router = express.Router()
 const ObjectId = require('mongoose').Types.ObjectId
 
+
 router.get('/company/', wrap(async (req, res) => {
-    const orders = await Order.find()
-    res.status(200).send({ data: orders })
+    const companies = await Company.find()
+    res.status(200).send({ data: companies })
 }))
 
 
 router.get('/company/:id', wrap(async (req, res) => {
     if (!ObjectId.isValid(req.params.id)) return res.status(400).send({ error: `Invalid id provided: ${req.params.id}` })
 
-    const order = await Order.findById(req.params.id)
-    if (!order) return res.status(400).send({ error: `Order Not Found` })
-    res.status(200).send({ data: order })
+    const company = await Company.findById(req.params.id)
+    if (!company) return res.status(400).send({ error: `Company Not Found` })
+    res.status(200).send({ data: company })
 }))
 
 
 router.post('/company', wrap(async (req, res) => {
-    const order = new Order({ ...req.body })
+    const company = new Company({ ...req.body })
 
-    const invalid = order.validateSync()
+    const invalid = company.validateSync()
     if (invalid) return res.status(400).send({ error: invalid })
 
-    const saved = await order.save()
+    const saved = await company.save()
     res.status(201).send({ data: saved })
 }))
 
@@ -36,15 +37,15 @@ router.post('/company', wrap(async (req, res) => {
 router.put('/company/:id', wrap(async (req, res) => {
     if (!ObjectId.isValid(req.params.id)) return res.status(400).send({ error: `Invalid id provided: ${req.params.id}` })
 
-    const order = await Order.findById(req.params.id)
-    if (!order) return res.status(400).send({ error: `Order Not Found` })
+    const company = await Company.findById(req.params.id)
+    if (!company) return res.status(400).send({ error: `Company Not Found` })
 
-    _assignIn(order, req.body)
+    _assignIn(company, req.body)
 
-    const invalid = order.validateSync()
+    const invalid = company.validateSync()
     if (invalid) return res.status(400).send({ error: invalid })
 
-    const saved = await order.save()
+    const saved = await company.save()
     res.status(200).send({ data: saved })
 }))
 
@@ -52,11 +53,11 @@ router.put('/company/:id', wrap(async (req, res) => {
 router.delete('/company/:id', wrap(async (req, res) => {
     if (!ObjectId.isValid(req.params.id)) return res.status(400).send({ error: `Invalid id provided: ${req.params.id}` })
 
-    const order = await Order.findById(req.params.id)
-    if (!order) return res.status(400).send({ error: `Order Not Found` })
+    const company = await Company.findById(req.params.id)
+    if (!company) return res.status(400).send({ error: `Company Not Found` })
 
-    await order.remove()
-    res.status(200).send({ message: `Order deleted` })
+    await company.remove()
+    res.status(200).send({ message: `Company deleted` })
 }))
 
 
