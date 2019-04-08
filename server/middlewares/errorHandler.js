8d35ed6478dd88b28d2b9a05d4abd22758508903
PyrozhenkoSerhii <serhii.pyrozhenko@nure.ua>
2019-04-08 22:01:14 +0300

    mongoose duplication error handler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,9 +1,16 @@
 import logger from '../utils/logger'
 
+const DUPLICATE_MONGO_ERROR = 11000
 
 const errorHandler = (err, req, res, next) => {
     logger.error(err.stack);
-    res.status(500).send({error: `Error occured on the server. If you are owner, check the logs of the server`});
+    if (err.code === DUPLICATE_MONGO_ERROR) {
+        let field = err.errmsg.split(".$")[1];
+        field = field.split(" dup key")[0];
+        field = field.substring(0, field.lastIndexOf("_"));
+        return res.status(400).send({ error: `This ${field} already exists` })
+    }
+    res.status(500).send({ error: `Error occured on the server. If you are owner, check the logs of the server` });
 }
 
 export default errorHandler
