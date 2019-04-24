5fd71d99416d5f256d6ad2e8f6b6f91b2961dc05
PyrozhenkoSerhii <serhii.pyrozhenko@nure.ua>
2019-04-24 10:42:09 +0300

    withAuthentication switched to currying

--- src/react/distributor/withAuthentication.js
+++ src/react/distributor/withAuthentication.js
@@ -4,18 +4,17 @@ import Authentication from './authentication'
 import globalContext from '../tools/state/context/global-context'
 
 
-const withAuthentication = (entity, callbackUrl, Component) => {
-    const withContextComponent = () => {
-        const context = useContext(globalContext)
+// used currying to return a functional component which can use hooks 
+const withAuthentication = (entity, callbackUrl, Component) => () => {
+    const context = useContext(globalContext)
 
-        if (!context.accounts.info[entity]) {
-            return <Authentication callbackUrl={callbackUrl} entity={entity} />
-        }
-
-        return <Component />
+    if (!context.accounts.info[entity]) {
+        return <Authentication callbackUrl={callbackUrl} entity={entity} />
     }
-    return withContextComponent
+
+    return <Component />
 }
+
 
 
 export default withAuthentication
